d09f05a43a459719c1c20659217bf4b596600838
Ryder Trew <95991015+NoxiousHypha@users.noreply.github.com>
2024-11-05 23:31:54 -0500

    Added comments
    Purple Toupee

--- PiBModel.py
+++ PiBModel.py
@@ -5,7 +5,7 @@ import scipy.constants as con
 # multi-ringed hydrocarbons (napthelene, anthracene, tetracene)
 # for comparison against lab-reported absorption spectra
 
-class HCRing:                       # We
+class HCRing:                       # Defines a molecule of linear aromatic rings
     def __init__(self, n):
         self.dbonds = 2 * n + 1     # Double-bonds of linear rings
         self.homo = self.dbonds     # Highest occupied molecular orbital
@@ -14,19 +14,19 @@ class HCRing:                       # We
         self.lx = (0.24e-9) * n     # Chain width
 
 
-def listEnergies(molecule):
+def listEnergies(molecule):         # Accesses an instance of HCRing and does PiaB modelling of its energy states
     E0 = (con.hbar**2 * con.pi**2)/(2*con.m_e)
     earray = []
     nxarray = []
     nyarray = []
-    for nx in range(1,molecule.lumo + 3):
-        for ny in range(1,4):
+    for nx in range(1,molecule.lumo + 3):    #To capture all energy levels, as nx increases energy by small amounts,
+        for ny in range(1,4):                #While ny increases energy by much larger amounts.
             energy = (E0 * ((nx**2 / (molecule.lx)**2) + (ny**2 / (molecule.ly)**2)))/con.e
             earray.append([energy])
             nxarray.append([nx])
             nyarray.append([ny])
     a = np.hstack([nxarray,nyarray,earray])
-    sort = a[a[:,2].argsort()]
+    sort = a[a[:,2].argsort()]               #Stacking and sorting syntax took a while to get the desired outpit
     return sort
 
 naphthelene = HCRing(2)
@@ -37,6 +37,6 @@ nEnergies = listEnergies(naphthelene)
 aEnergies = listEnergies(anthracene)
 tEnergies = listEnergies(tetracene)
 
-np.savetxt('./naphtheleneEnergies.txt',nEnergies, delimiter=', ', header='Napthelene Calculated Energies, (nx, ny, energy)')
+np.savetxt('naphtheleneEnergies.txt',nEnergies, delimiter=', ', header='Napthelene Calculated Energies, (nx, ny, energy)')
 np.savetxt('anthraceneEnergies.txt',aEnergies, delimiter=', ', header='Anthracene Calculated Energies, (nx, ny, energy)')
 np.savetxt('tetraceneEnergies.txt',tEnergies, delimiter=', ', header='Tetracene Calculated Energies, (nx, ny, energy)')
